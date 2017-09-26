0a3c0830c7be56a6105ddc5630baa49b4cff4083
Alexey Savinov <belsat.net@gmail.com>
2017-09-26 16:57:37 +0300

    find duplicates

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Find duplicate of files on directory tree (by name and file size)
+
+Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python duplicates.py c:\adb
+```
+
+##Output example:
+
+```bash
+$ python duplicates.py c:\adb
+Duplicate: c:\adb\test\adb.exe file size: 0
+Duplicate: c:\adb\test2\adb.exe file size: 0
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,32 @@
+import os
+import sys
+
+
+def find_file_duplicates(path_to_dir):
+    dict_of_files = {}
+    dict_of_duplicates = {}
+    for dirpath, subdirs, filenames in os.walk(path_to_dir):
+        for file in filenames:
+            file_size = os.stat(os.path.join(dirpath, file)).st_size
+            file_path = os.path.join(dirpath, file)
+            if dict_of_files.get(file) == None:
+                dict_of_files.update({file: file_size})
+            elif dict_of_files.get(file) == file_size:
+                dict_of_duplicates.update({file_path: file_size})
+            else:
+                pass
+    return dict_of_duplicates
+
+
+def print_file_duplicates(files_dict):
+    for file in files_dict.keys():
+        print('Duplicate: ' + file + ' file size: ' + str(files_dict.get(file)))
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        path_to_dir = sys.argv[1]
+        files_dict = find_file_duplicates(path_to_dir)
+        print_file_duplicates(files_dict)
+    else:
+        print('Please define path to dir for analyse \nExample: python duplicates.py <path_to_dir>')
